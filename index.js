e91a850c3540acdadefb1e1511181c333c1061a7
Andrew Petushkou <gomel.coder@gmail.com>
2018-04-10 22:13:01 +0300

    Edit String

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const TOKEN = '580307062:AAHG55gWQtMDad9hTF-6ZqlRlMKyAIwKTO8'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Hello from HEROKU, bot says :"Hi, ${msg.from.first_name}"')
+    bot.sendMessage(msg.chat.id, 'Hello from HEROKU, bot says :"Hi,' + msg.from.first_name + '"')
 })
