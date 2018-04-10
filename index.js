4ccc84cd6a9170b081f0e7af8617e5fece9695e7
Andrew Petushkou <gomel.coder@gmail.com>
2018-04-10 22:17:46 +0300

    Edit String

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const TelegramBot = require('node-telegram-bot-api')
+const TelegramBot = require('node-telegram-bot-api');
 
-const TOKEN = '580307062:AAHG55gWQtMDad9hTF-6ZqlRlMKyAIwKTO8'
+const TOKEN = '580307062:AAHG55gWQtMDad9hTF-6ZqlRlMKyAIwKTO8';
 
-const bot = new TelegramBot(TOKEN, {polling: true})
+const bot = new TelegramBot(TOKEN, {polling: true});
 
 bot.on('message', msg => {
     bot.sendMessage(msg.chat.id, 'Hello from HEROKU, bot says :"Hi,' + msg.from.first_name + '"')
-})
+});
