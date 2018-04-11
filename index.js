8490ab1a7f48a893ea16dd81c52a90fe886fbeaf
Andrew Petushkou <gomel.coder@gmail.com>
2018-04-11 19:53:24 +0300

    Edit String

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const TelegramBot = require('node-telegram-bot-api');
-
-const TOKEN = '580307062:AAHG55gWQtMDad9hTF-6ZqlRlMKyAIwKTO8';
-var port = process.env.PORT || 3000;
-const bot = new TelegramBot(TOKEN, {
+const TOKEN = process.env.TELEGRAM_TOKEN || '580307062:AAHG55gWQtMDad9hTF-6ZqlRlMKyAIwKTO8';
+const options = {
     webHook: {
-        port: port,
-        autoOpen: false
+        port: process.env.PORT
     }
-});
+};
+// See: https://devcenter.heroku.com/articles/dyno-metadata
+const url = process.env.APP_URL || 'https://heroku-tg-bot.herokuapp.com/';
+const bot = new TelegramBot(TOKEN, options);
 
-bot.openWebHook()
-bot.setWebHook('https://heroku-tg-bot.herokuapp.com/'+ TOKEN)
+// This informs the Telegram servers of the new webhook.
+bot.setWebHook(`${url}/bot${TOKEN}`);
 
-bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Hello from HEROKU, bot says :"Hi,' + msg.from.first_name + '"')
+// Just to ping!
+bot.on('message', function onMessage(msg) {
+    bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
 });
