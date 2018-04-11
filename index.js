2288c0f1c6d0f2441dfd2ac9192fee9d10b08e05
Andrew Petushkou <gomel.coder@gmail.com>
2018-04-11 19:46:26 +0300

    Edit String

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 const TOKEN = '580307062:AAHG55gWQtMDad9hTF-6ZqlRlMKyAIwKTO8';
-
+var port = process.env.PORT || 3000;
 const bot = new TelegramBot(TOKEN, {
     webHook: {
-        port: '3000',
+        port: port,
         autoOpen: false
     }
 });
 
 bot.openWebHook()
-bot.setWebHook('https://heroku-tg-bot.herokuapp.com/bot/'+ TOKEN)
+bot.setWebHook('https://heroku-tg-bot.herokuapp.com/'+ TOKEN)
 
 bot.on('message', msg => {
     bot.sendMessage(msg.chat.id, 'Hello from HEROKU, bot says :"Hi,' + msg.from.first_name + '"')
